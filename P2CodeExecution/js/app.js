5bded1f78481b394ca9e7a9f3ac21b68a54e718d
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-12 11:21:06 +0200

    Created a new project named P2CodeExecution

---
+++ P2CodeExecution/js/app.js
@@ -0,0 +1,14 @@
+console.log("This is line 1.");
+console.log("This is line 2.");
+console.log("This is line 3.");
+console.log("This is line 4.");
+console.log("This is line 5.");
+console.log("This is line 6");
+console.log("This is line 7");
+
+let hoursInClass = 5;
+let minutesInClass = 60;
+
+console.log(hoursInClass);
+console.log(minutesInClass);
+console.log(hoursInClass + minutesInClass);
