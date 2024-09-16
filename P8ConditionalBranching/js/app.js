8f52f1d835e1c4d71b6727863af68ccff42bae91
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-16 13:34:28 +0200

    Added CB Exercise

--- P8ConditionalBranching/js/app.js
+++ P8ConditionalBranching/js/app.js
@@ -1,7 +1,9 @@
-const birthYear = 2010;
+const birthYear = 2003;
 const currentYear = 2024;
 let ageOfPerson = currentYear - birthYear;
 console.log(ageOfPerson);
+
+const favoriteNumber = 21;
 
 // Save to a let variable named isChild, when the age is from 0 till 10.
 let isChild = (ageOfPerson >= 0) && (ageOfPerson <= 10);
@@ -12,6 +14,32 @@ let isTeenager = (ageOfPerson >= 11) && (ageOfPerson <= 17);
 // Save to a let variable named isAdult, when the age is 18 or more.
 let isAdult =  ageOfPerson >= 18;
 
-console.log(isChild);
-console.log(isTeenager);
-console.log(isAdult);
+if (isChild) {
+  console.log("You are a child");
+}
+if (isTeenager) {
+  console.log("You are a teenager");
+}
+else if (isAdult) {
+  console.log("You are an adult");
+}
+console.log(favoriteNumber);
+
+if (favoriteNumber > ageOfPerson) {
+  console.log("favorite number is greater");
+}
+
+else if (favoriteNumber === ageOfPerson) {
+  console.log("they are the same");
+}
+
+else {
+  console.log("age is greater");
+}
+
+
+
+// console.log("You are a child");
+// console.log("You are a teenager");
+// console.log("You are an adult");
+
