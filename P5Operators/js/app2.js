0e6405f0573dec7eaa37b83dc93e79ad5c196e30
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-16 09:40:46 +0200

    Added the last 2 Operates in App2

--- P5Operators/js/app2.js
+++ P5Operators/js/app2.js
@@ -24,15 +24,38 @@ console.log(area);
 // negation = numberOne - 22;
 // console.log(negation); // Reconized this is right, but still wrong.
 
+let negationNumber = 11;
+let negatedOutput;
+negatedOutput = negationNumber * - 1;
+console.log(negatedOutput);
+
+
 let weight = 70;
 let height = 1.82;
-output = weight / (height * height);
-console.log(output);
+// output = weight / (height * height); How I solved it
+bmi = weight / Math.pow(height, 2);
+console.log(bmi);
 
+let sideA = 3;
+let sideB = 4;
+let hypotenuse;
 
+let squareOfAAndB = Math.pow(sideA, 2) + Math.pow(sideB, 2);
+hypotenuse = Math.sqrt(squareOfAAndB);
+console.log(hypotenuse);
 
+let secondsInput = 111;
+let minSecOutput;
 
+let minOutput;
+minOutput = Math.floor(secondsInput / 60);
+console.log(minOutput);
 
+let secOutput;
+secOutput = secondsInput - minOutput + 60;
+console.log(secOutput);
+minSecOutput = minOutput + "minute(s) and" + secOutput + "second(s)";
+console.log(minSecOutput);
 
 
 
