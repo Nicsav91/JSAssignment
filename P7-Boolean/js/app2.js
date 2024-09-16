2a3a362d9d751f8dc4fa6ca2f0e463861d48dfda
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-16 11:49:12 +0200

    Created App2 with Booleans

---
+++ P7-Boolean/js/app2.js
@@ -0,0 +1,18 @@
+let playerTouchGround = false;
+
+let playerHasJetPack = true;
+
+if (playerTouchGround) {
+  console.log("Player is on the ground");
+  console.log("Player Can Jump");
+}
+
+else if (playerHasJetPack) {
+  console.log("Player can Jet Away");
+}
+
+else {
+  console.log("Player is NOT on the ground");
+  console.log("Player CANNOT jump");
+}
+
