00307cd22e8707885fb4cf2cad7a583400a3b992
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-17 11:02:04 +0200

    Completed the P10 Input Exercises

--- P10InputsAndOutputs/js/app.js
+++ P10InputsAndOutputs/js/app.js
@@ -10,3 +10,49 @@ function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
 console.log(add(5, 20));
+
+function averageNumber(firstNumber, secondNumber) {
+  // let sumOfNumbers = firstNumber + secondNumber;
+  // return sumOfNumbers / 2; För att bryta ner uträkningen, kan bli mer läsbart.
+  return (firstNumber + secondNumber) / 2;
+}
+console.log(averageNumber(2, 6));
+
+function welcomingResult(firstString, secondString) {
+  // let greeting = "Welcome " + firstString + " " + secondString Den kommer klaga men man kan göra så också.
+  return "Welcome" + firstString + secondString;
+  // return "Welcome " + firstString + " " + secondString; How Ben did it
+}
+console.log(welcomingResult(" Ben", " Ting!"));
+
+function randomNumber() {
+  // return Math.floor(Math.random() * 3);
+  return Math.floor(Math.random() * Math.floor(4));
+}
+console.log(randomNumber());
+
+function numberToString(firstNumber) {
+if (firstNumber === 0){
+  return "A";
+}
+if (firstNumber === 1){
+  return "B";
+}
+if (firstNumber === 2){
+  return "C";
+}
+}
+console.log(numberToString(2));
+
+function equalNumbers(firstNumber, secondNumber) {
+  if (firstNumber === secondNumber){
+    return "Both numbers are equal!";
+  }
+  if (firstNumber > secondNumber){
+    return "First number is greater!";
+  }
+  if (firstNumber < secondNumber){
+    return "Second number is greater!";
+  }
+}
+console.log(equalNumbers(5, 5));
