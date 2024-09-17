c3d81b9c6210cfebebc902e60e607a60cb80d59b
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-17 09:50:22 +0200

    Added functions with Inputs

---
+++ P10InputsAndOutputs/js/app.js
@@ -0,0 +1,12 @@
+// let inputNumberOne = 0;
+
+function multiplyBySelf(numberToMultiply) {
+return numberToMultiply * numberToMultiply;
+}
+
+console.log(multiplyBySelf(4));
+
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+console.log(add(5, 20));
