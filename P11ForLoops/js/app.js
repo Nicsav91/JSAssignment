193a0c7eb18641264b4f22e3d5a22b30376270dd
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-17 13:13:44 +0200

    Created a New Loop Project

---
+++ P11ForLoops/js/app.js
@@ -0,0 +1,11 @@
+// for (let i = 0; i < 5 ; i++) {
+ // console.log(i);
+// }
+
+// for (let u = 1; u < 6; u++) {
+  // console.log(u * Math.floor(Math.random() * 3));
+// }
+
+for (let h = 1; h <= 2024; h**) {
+  console.log(h * Math.pow(1, 2024));
+}
