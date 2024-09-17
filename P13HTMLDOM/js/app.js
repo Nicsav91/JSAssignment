89078c2f8577ff599e0b7a73dec62ae8465aefbb
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-17 13:57:53 +0200

    Created a JS reference

---
+++ P13HTMLDOM/js/app.js
@@ -0,0 +1,8 @@
+// Reference "text0" and assign it to the variable called "textField0"
+const textField0 = document.getElementById("text0");
+
+// Ändrar texten från Hola Hola Peaches till Nicolina i browsern
+textField0.innerHTML = "Nicolina";
+
+// Ändrar färgen på Nicolina till rosa
+textField0.style.color = "pink";
