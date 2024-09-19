34aa387c1758c9d4b9d0a1838c8fc13f86e82998
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-19 13:37:12 +0200

    Uppdated comments

--- playGround2/js/app.js
+++ playGround2/js/app.js
@@ -1,24 +1,36 @@
+// Document Elements 
 // Looks for "text0". Assign that to the variable called textField0.
 const textField0 = document.getElementById('text0');
 
 // Buttons
 const increaseScoreButton = document.getElementById('button0');
-// Internal Variables
+
+// Internal Variables,
 let score = 0;
 
 // Process (what is going to happen in what sequence?)
 increaseScoreButton.addEventListener('click', () => {
   increaseScoreByOne();
   updateScoreText();
+  checkScoreForSeven();
 })
 
-// Controllers
+// Controllers, kalkylerar
 function increaseScoreByOne() {
   score++;
-  updateScoreText();
+}
+
+function checkScoreForSeven () {
+ if (score >= 7) {
+   changeScoreTextColorToGreen();
+ }
 }
 
 // View
 function updateScoreText() {
-  textField0.innerText = score;
+  textField0.innerHTML = "Your score is: " + score;
+}
+
+function changeScoreTextColorToGreen() {
+  textField0.style.color = "green";
 }
