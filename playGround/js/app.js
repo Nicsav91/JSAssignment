df5a6c8d27d62a43211570761c7ae9fa0dcbb566
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-19 09:59:48 +0200

    Linked HTML to JS app and Created a Clickable Button

---
+++ playGround/js/app.js
@@ -0,0 +1,17 @@
+ const firstText = document.getElementById("firstText");
+firstText.innerHTML = "Nicolinas Page";
+firstText.style.color = "Green";
+
+// const firstButton = document.getElementById("firstButton");
+ 
+const firstButton = document.querySelector("#firstButton");
+firstButton.innerHTML = "Click me!";
+firstButton.style.color = "pink";
+
+firstButton.addEventListener('click', () => {
+  console.log('button clicked!');
+ });
+
+
+
+
