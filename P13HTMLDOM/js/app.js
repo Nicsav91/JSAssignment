cfb6c1e1be3a55f3e41419cf8b22d25683ed9c85
Nicsav91 <savmarkernicolina@gmail.com>
2024-09-19 09:26:41 +0200

    Made unfinished functions to comments

--- P13HTMLDOM/js/app.js
+++ P13HTMLDOM/js/app.js
@@ -10,31 +10,32 @@ const textField1 = document.getElementById("text1");
 textField1.innerHTML = "holaa";
 
 
-// const button0 = document.createElement("button0");
+const button0 = document.createElement("button0");
 
 
-// function multiplyBySelf(numberToMultiply) {
- // return numberToMultiply * numberToMultiply;
-// }
+function multiplyBySelf(numberToMultiply) {
+  return numberToMultiply * numberToMultiply;
+}
 
-//function averageNumber(firstNumber, secondNumber) {
-  // let sumOfNumbers = firstNumber + secondNumber;
+
+function averageNumber(firstNumber, secondNumber) {
+  //let sumOfNumbers = firstNumber + secondNumber;
   // return sumOfNumbers / 2; För att bryta ner uträkningen, kan bli mer läsbart.
-  // return (firstNumber + secondNumber) / 2;
+ return (firstNumber + secondNumber) / 2;
+}
+
+// let score
+// function increaseBy(inputNumber) {
+//  return score += inputNumber;
 // }
 
-let score
-function increaseBy(inputNumber) {
-  return score += inputNumber;
-}
+// const textField2 = document.getElementById("text");
 
-const textField2 = document.getElementById("text");
+// function updateScore() {
+//  textField2.innerHTML = score;
+//}
 
-function updateScore() {
-  textField2.innerHTML = score;
-}
+// const button1 = document.getElementbyId("button");
+// button1.addEventListener("click", increaseBy);
 
-const button1 = document.getElementbyId("button");
-button1.addEventListener("click", increaseBy);
-
-updateScore.addEventListener("click", updateScore);
+// updateScore.addEventListener("click", updateScore);
